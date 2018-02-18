0f07dd1d5c0cecc39d8df9374c82bbe725dae8f1
szczot <iszczot@gmail.com>
2018-02-18 13:50:39 -0500

    Prevents admin from deleting own account in the ConfirmDeleteServlet

--- src/servlets/ConfirmDeleteServlet.java
+++ src/servlets/ConfirmDeleteServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSession;
 
 import daoimpl.UserProfileDaoImpl;
 import models.UserProfile;
@@ -19,8 +19,11 @@ import models.UserProfile;
 @WebServlet("/ConfirmDeleteServlet")
 public class ConfirmDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-//	private HttpSession session = null;
 	private int id;
+	private HttpSession session;
+	// variable that will determine whether the user is authorized to delete profile
+	// deny by default
+	private boolean allowDelete = false;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -47,13 +50,42 @@ public class ConfirmDeleteServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		this.id = Integer.parseInt(request.getParameter("id"));
-		// session = request.getSession(true);
-		UserProfileDaoImpl userProfileDaoImpl = new UserProfileDaoImpl();
-		UserProfile deleteProfile = userProfileDaoImpl.accessProfile(this.id);
-		// session.setAttribute("ownProfile", deleteProfile);
-		request.setAttribute("profileToDelete", deleteProfile);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("delete.jsp");
-		dispatcher.forward(request, response);
+		// if the user is not admin he/she should not be allowed to delete
+		// any profile
+		// Therefore, retrieve role and own id from session variables:
+		session = request.getSession(true);
+		String role = (String) session.getAttribute("role");
+		UserProfile loggedInUser = (UserProfile) session.getAttribute("ownProfile");
+		int idOfLoggedInUser = loggedInUser.getId();
+		if (!role.equals("admin")) {
+			// deny
+			allowDelete = false;
+		} // end if user was not admin
+		else { // if user is admin
+				// if user is admin check if the posted id equals own id
+			if (this.id == idOfLoggedInUser) {
+				// if posted id equals own id, it means that admin user wants to delete own
+				// profile. In such case we should forbid the deletion
+				allowDelete = false;
+			} else {
+				// this means that admin user wants to delete someone else profile
+				// authorize him/her to do that
+				allowDelete = true;
+			}
+		} // end if user was admin
+			// now verify if user is authorized to delete profile
+		if (allowDelete) {
+			UserProfileDaoImpl userProfileDaoImpl = new UserProfileDaoImpl();
+			UserProfile deleteProfile = userProfileDaoImpl.accessProfile(this.id);
+			// session.setAttribute("ownProfile", deleteProfile);
+			request.setAttribute("profileToDelete", deleteProfile);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("delete.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			request.setAttribute("ErrorMessage", "You are not authorized to delete this account!");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
 }
